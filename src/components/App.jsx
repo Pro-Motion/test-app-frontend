93483d8ef4329738d24170519845da06d8425f1e
Vladislav-UZH <perebziak.vladislav@gmail.com>
2023-02-21 21:54:35 +0200

    project initialization

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import axios from 'axios';
+
 export const App = () => {
+  // 187.213.75.212:8080//api/get/civil/2ee5fa4a-61ea-41ed-9cf2-d144df2dabc2
+  const getCivilData = async () => {
+    const resp = await axios.get(
+      '187.213.75.212:8080/api/get/civil/2ee5fa4a-61ea-41ed-9cf2-d144df2dabc2'
+    );
+    console.log(resp);
+    // const data = resp.json();
+    // console.log(data);
+  };
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <button disabled={true} onClick={getCivilData}>
+        get data
+      </button>
     </div>
   );
 };
